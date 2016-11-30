81b4ab2f2154592851baf1aed86edd1002e23ff7
Frederick <duelarm@gmail.com>
2016-11-30 20:04:42 +0000

    renaming and cleaning up code

--- public/js/panel.js
+++ public/js/panel.js
@@ -1,64 +1,37 @@
-// Define the `phonecatApp` module
 var phonecatApp = angular.module('phonecatApp', ["chart.js"])
-    .config(function(ChartJsProvider) {
-        // Configure all charts
-        ChartJsProvider.setOptions({
-            responsive: false
-        });
-    })
-    .config(function($interpolateProvider) {
-        $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-    });
 
-// Define the `PhoneListController` controller on the `phonecatApp` module
-phonecatApp.controller('PhoneListController', function PhoneListController($scope, $window, $http, $timeout, objectmaker, poster,datasetter) {
+    .config(function(ChartJsProvider) {ChartJsProvider.setOptions({responsive: false});})
+    .config(function($interpolateProvider) {$interpolateProvider.startSymbol('{[{').endSymbol('}]}');});
+
+phonecatApp.controller('PhoneListController', function PhoneListController($scope, $window, $http, object, poster,datasetter) {
 
     var baseUrl = $window.base;
-
     $scope.labels = ["Failed", "Passed", "Not Run", "Blocked", "Caution"];
     $scope.colors = ['#f47457', '#7eff7a', '#e0e0e0', '#f4f356', '#f29e56'];
-
-    AP.resize(); //resized iframe to proper width
-
-    //Getting JIRA issue and project keys 
+    
+    AP.resize(); 
     AP.require("_util", function(util) {
         
-        // full URL of host app
+        //Getting project and issue key querystring params from host URL
         var hostString = util.decodeQueryComponent(window.location.href);
-
-        //query strings are parsed with URI.js
         var qs = URI(hostString).query(true);
-
         $scope.issueKey = qs['issueKey'];
-
         $scope.projectKey = qs['projectKey'];
-
-        //Getting SpiraTeam Config info (stored in JIRA project config settings)
+        
+        //requesting stored SpiraTeam API Acsess config from JIRA 
         AP.require(['request'], function(request) {
-            request({
-                url: baseUrl + '/rest/api/latest/project/' + $scope.projectKey + '/properties/spira',
+            request({url: baseUrl + '/rest/api/latest/project/' + $scope.projectKey + '/properties/spira',
                 success: function(response) {
-                    // Convert the string response to JSON
                     response = JSON.parse(response);
-                    var requestdata = objectmaker.objectmaker(response,$scope.issueKey)
-
-                    poster.postmaker($http, requestdata).then(function successCallback(response) {
                     
+                    //factory driven AJAX calls to get Spriteam Requirement data (objectmaker,poster,datasetter)
+                    var requestdata = object.objectmaker(response,$scope.issueKey)
+                    poster.postmaker($http, requestdata).then(function successCallback(response) {
                     $scope.data = datasetter.set(response)
-                   
-                    /*
-                    //basic math operation
-                        var notRun = response.data.CoverageCountTotal - (response.data.CoverageCountPassed + response.data.CoverageCountFailed + response.data.CoverageCountCaution + response.data.CoverageCountBlocked)
-                     //assign data value to resulting array    
-                         = [response.data.CoverageCountFailed, response.data.CoverageCountPassed, notRun, response.data.CoverageCountBlocked, response.data.CoverageCountCaution]
-*/
                     }, function errorCallback(response) {
-
                         return response
                     });
-
                 }
-
             });
 
         });
@@ -67,7 +40,7 @@ phonecatApp.controller('PhoneListController', function PhoneListController($scop
 
 });
 
-phonecatApp.factory("objectmaker", function() {
+phonecatApp.factory("object", function() {
 
     return {
 
